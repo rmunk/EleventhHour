4bb5bf7a414f8f974449a3d66c893d3613ed73d9
rmunk <ranko.munk@gmail.com>
2017-03-07 12:51:18 +0100

    Removed splash activity; Styles cleanup; Maps api token moved to common

--- auth/src/main/java/hr/nas2skupa/eleventhhour/auth/SignInActivity.java
+++ auth/src/main/java/hr/nas2skupa/eleventhhour/auth/SignInActivity.java
@@ -23,7 +23,6 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
 
         auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
